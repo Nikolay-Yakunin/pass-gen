aff1009fba03ccc9c1ee8c8fd40612a974562f19
nissasyb <akuninn52@gmail.com>
2025-04-07 22:20:19 +0700

    Закомитил тестовую страницу

--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, CopyText } from "../../components/shared";
+import { generatePassword } from "../../models/Password/password";
 
 function TestPage() {
+  const [pass, setPass] = useState();
+  const [complexity, setComplexity] = useState("low");
+
+  const handleGenPass = () => {
+    setPass(generatePassword(12, complexity));
+  };
+
+  const handlerComplexity = (level) => {
+    setComplexity(level);
+  };
+
   return (
     <main>
       <h1>Test Page</h1>
       <section>
-        <h1>shared ui</h1>
         <div className="row">
-            <Button>Click Me</Button>
-            <CopyText>Copy Me</CopyText>
+          <h2>Shared ui</h2>
+          <Button>Click Me</Button>
+          <CopyText>Copy Me</CopyText>
+        </div>
+        <div className="row">
+          <h2>Pass gen</h2>
+          <span>{pass ? pass : "Тут будет пароль..."}</span>
+          <div>
+            <button onClick={() => handlerComplexity("low")}>Low</button>
+            <button onClick={() => handlerComplexity("medium")}>medium</button>
+            <button onClick={() => handlerComplexity("high")}>high</button>
+          </div>
+          <button onClick={handleGenPass}>Gen pass</button>
         </div>
       </section>
     </main>
