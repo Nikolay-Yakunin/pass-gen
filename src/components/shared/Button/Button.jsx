71d07ee7ae4bb35a73d243b517b864f3de9c0f96
nissasyb <akuninn52@gmail.com>
2025-04-07 20:02:52 +0700

    Ref button - add export

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import styles from './button.module.css';
+import styles from './Button.module.css';
 
 /**
  * @brief Компонент кнопки, принимает текст или разметку
  * @param {*} props 
  * @returns jsx
  */
-const Button = (props) => {
+export const Button = (props) => {
     return (
         <button className={`${styles.button}`}>{props}</button>
     )
 }
+
